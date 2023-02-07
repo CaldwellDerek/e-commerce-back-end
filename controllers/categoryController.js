5569caf071066d7d19e9d43c6f5fb22a591d9528
Derek Caldwell <djohncaldwell@gmail.com>
2023-02-07 11:53:43 -0800

    update Category by ID route added

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -31,13 +31,34 @@ router.post("/", async (request, response)=> {
     try {
         const newCategory = await Category.create({
             category_name: request.body.category_name
-        })
+        });
         response.status(200).json(newCategory);
     } catch (error){
         console.log(error);
         response.status(500).json({msg: "Something went wrong!"});
     }
-})
+});
+
+router.put("/:id", async (request, response)=> {
+    try {
+        const updateCategory = await Category.update({
+            category_name: request.body.category_name
+        },
+        {
+            where: {
+                id: request.params.id
+            }
+        });
+        if (updateCategory){
+            response.status(200).json(updateCategory);
+        } else {
+            response.status(404).json({msg: "Category not found!"});
+        }
+    } catch (error){
+        console.log(error);
+        response.status(500).json({msg: "Something went wrong!"});
+    }
+});
 
 module.exports = router;
 
