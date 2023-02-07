052cd3fc764ef71a758f90b3b92d25add5d96454
Derek Caldwell <djohncaldwell@gmail.com>
2023-02-07 11:39:45 -0800

    find category by id route added

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,8 +7,24 @@ router.get("/", async (request, response)=> {
         const allCategories = await Category.findAll();
         response.status(200).json(allCategories);
     } catch (error) {
+        console.log(error);
         response.status(500).json({msg: "Something went wrong!"});
     }
-})
+});
+
+router.get("/:id", async (request, response)=> {
+    try {
+        const category = await Category.findByPk(request.params.id);
+        console.log(category);
+        if (category){
+            response.status(200).json(category);
+        } else {
+            response.status(404).json({msg: "Category not found!"});
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({msg: "Something went wrong!"});
+    }
+});
 
 module.exports = router;
