09a904f0fed9240bbe556572c14ed0008a3b9e69
Derek Caldwell <djohncaldwell@gmail.com>
2023-02-07 11:44:27 -0800

    find Tag by ID route added

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -11,4 +11,20 @@ router.get("/", async (request, response)=> {
     }
 })
 
+router.get("/:id", async (request, response)=> {
+    try {
+        const tag = await Tag.findByPk(request.params.id);
+        console.log(tag);
+        if (tag){
+            response.status(200).json(tag);
+        } else {
+            response.status(404).json({msg: "Tag not found!"});
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({msg: "Something went wrong!"});
+    }
+});
+
+
 module.exports = router;
